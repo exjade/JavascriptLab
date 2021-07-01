275e5d520b52baaa6bdfc8510ae6d5eb7680edc9
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-01 09:12:01 -0600

    Aprendí el primer tipo de variable: VAR

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,20 @@
+// Las variables son una caracteristica de cualquier lenguaje de programación. Si un lenguaje tiene variables es un lenguaje de programación
+
+// Existen 3 maneras de crear variables en JavaScript, aunque en las nuevas versiones sólo de utilizan dos
+
+// debes usar la palabra reservada var | Significa que conoce ese lenguaje
+// Variable nombredevariable asignarvalor (=) | Un valor se asigna con el signo de igual = | También se le llama inicializar una variable con un valor
+
+var producto = "Monitor de 24 pulgadas";
+console.log(producto);
+
+// Las variables se pueden reasignar | Se pueden usar tanto comillas dobles y sencillas pero no se pueden mezclar
+
+producto = 'Monitor de 19 pulgadas'; 
+console.log(producto);
+//Javascript es un lenguaje de tipo dinamico  | No es un lenguaje que tenga Tipado. Si quieres tipado existe algo llamado TYPESCRIPT
+
+producto = 20; 
+console.log(producto);
+
+
