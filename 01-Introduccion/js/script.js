01557b85b7d09ca10c442022134be28afdb75a0f
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-01 08:21:13 -0600

    Hicimos un código que pregunta el nombre y muestra que estamos aprendiendo Javascript

--- 01-Introduccion/js/script.js
+++ 01-Introduccion/js/script.js
@@ -1,1 +1,7 @@
-alert("Hola Mundo");
+
+// alert("Hola Mundo");
+
+const nombre = prompt('Cual es tu nombre?');
+
+// Esto lo que te permite es seleccionar elementos del html
+document.querySelector('.contenido').innerHTML = '${nombre} está aprendiendo Javascript Moderno';
