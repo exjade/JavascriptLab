4ca6af2d1e7bc1643fcab7e5e13b391f34493baa
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-01 08:42:10 -0600

    Aprendimos diferentes formas de escribir en la consola

--- 01-Introduccion/comentar.js
+++
@@ -1,5 +0,0 @@
-/* Comentar el código puede ser para añadir una descripción de lo que hace y otra para que ese código no se ejecute
-Existen 2 formas de comentar código "//" sólo comentaria una línea de código
-Cuando se van a comentar bloques de líneas se utiliza /* */
-
-const producto = 'Monitor 24 pulgadas';
---
+++ 01-Introduccion/js/comentar.js
@@ -0,0 +1,15 @@
+/* 
+Comentar el código puede ser para añadir una descripción de lo que hace y otra para que ese código no se ejecute
+Existen 2 formas de comentar código "//" sólo comentaria una línea de código
+Cuando se van a comentar bloques de líneas se utiliza  
+*/
+
+// pregunta al usuario su nombre
+const nombre = prompt('Cual es tu nombre?'); //No es necesario comentar cada línea de código
+
+// Toma el nombre y lo muestra en pantalla - De está forma sé dentro de 6 meses qué es lo que hace está línea.
+document.querySelector('.contenido').innerHTML = '${nombre} está aprendiendo Javascript Moderno';
+
+
+// asgina el valor hacia la variable producto
+const producto = 'Monitor 24 pulgadas'; //este código está resaltado
