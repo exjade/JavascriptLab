f5385e937aa8d06404b6c7a0ad8e674e10218a38
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-02 07:51:35 -0600

    Crear números en JavaScript

---
+++ 04-Numeros/js/01-app.js
@@ -0,0 +1,53 @@
+
+/*
+const numero1 = 20;
+const numero2 = "40";
+
+console.log(numero1); // Esté está en azul/purpura - Significa que es un número
+console.log(numero2); // Está en color BLANCO/NEGRO - Significa que es un número pero su tipo de dato es una STRING
+*/
+
+// Creación de números
+
+/*
+    Existen diferentes formas de crear un NUMERO incluso en otros lenguajes se tratan de diferentes formas
+    Por ejemplo: Número entero, flotante, etc. 
+
+    * En JavaScript todos los NUMEROS se crean de la misma forma * 
+*/ 
+
+// Números enteros
+
+const numero1 = 30;
+const numero2 = 20;
+
+console.log(numero1);
+console.log(numero2);
+
+// Número flotante - En otros lenguajes tendrías que especificar que es de tipo FLOAT (Flotante) En JS no hay problema
+
+
+
+const numero3 = 20.20;
+const numero4 = .102030;
+
+console.log(numero3);
+console.log(numero4);
+
+/* 
+No tienes que especificar el tipo de dato aunque unos sean enteros, otros fracciones y otros una pequeña fraccion. 
+
+Comillas lo vuelve una STRING
+*/
+
+// Numeros Negativos - Todos los números se crean de la misma forma en JS
+
+const numero5 = -20;
+
+console.log(numero5);
+
+// Otra sintaxis es usando new Number();  | No es tan común
+
+const numero6 = new Number(80);
+console.log(numero6) // Obtendremos un OBJETO con un número 80
+
