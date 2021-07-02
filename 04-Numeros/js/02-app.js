b2d7c9c6014c7b8c53b2abe6ef1be1db883cdd33
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-02 08:20:20 -0600

    Aprendimos los operaciones básicas en Javascript

---
+++ 04-Numeros/js/02-app.js
@@ -0,0 +1,41 @@
+/*OPERACIONES EN JAVASCRIPT*/
+
+const numero1 = 30;
+const numero2 = 20;
+
+let resultado;
+
+// suma
+
+/* 
+Si lo recordamos cuando lo utilizamos con un STRING, lo concatena
+Pero en está ocasión dice: Ambos son numeros, los voy a sumar
+*/
+
+resultado = numero1 + numero2; 
+
+
+// resta
+
+resultado = numero1 - numero2; 
+
+// multiplicación
+
+resultado = numero1 * numero2; 
+
+// division
+
+resultado = numero1 / numero2; 
+
+// Modulo
+/*
+    El modulo, al dividir 30 / 20 buscará cuánto es el residuo (el sobrante), cuánto queda después de hacer lo división.
+
+    Se utiliza en algoritmos, estructuras de datos para practicar algunos ejercicios de programación
+
+*/
+
+resultado = numero1 % numero2; 
+
+console.log(resultado);
+
