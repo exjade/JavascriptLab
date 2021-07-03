3269a98a73113396defb2d0aa30ca95628d0368a
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-03 09:49:56 -0600

    Operador Mayor que y Menor que

---
+++ 05-Operadores/js/01-app.js
@@ -0,0 +1,16 @@
+const numero1 = 20;
+const numero2 = "20";
+const numero3 = 30;
+
+// Operador Mayor A
+
+console.log(numero1 > numero3); 
+console.log(numero3 > numero1);
+
+// Ejemplo: Si la persona tiene suficiente dinero en su tarjeta puede comprar si no lo tiene ocurrirá un error en su compra
+
+// Operador Menoa A
+
+console.log(numero1 < numero3); 
+console.log(numero3 < numero1);
+
