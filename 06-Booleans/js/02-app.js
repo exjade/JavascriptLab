f4073338a5ade983d61304781be788c49a4a086c
Adrian Zamora <adrianzamora10@outlook.com>
2021-07-03 11:00:10 -0600

    Al comparar un el tipo de dato BOOLEAN con una STRING dará false porque el tipo de dato no es igual

---
+++ 06-Booleans/js/02-app.js
@@ -0,0 +1,14 @@
+const boolean1 = true;
+const boolean2 = false;
+
+/*
+    Si comparamos los dos BOOLEANS nos dará FALSE porque el tipo de dato es el mismo pero el VALOR no lo es.
+*/
+
+console.log(boolean1 === boolean2);
+
+console.log(boolean1 === true);
+
+// El tipo de dato no es igual como al igual que ocurre con NUMEROS y STRINGS
+
+console.log(boolean1 === "true"); // Nos da error porque es una STRING y no un BOOLEAN
